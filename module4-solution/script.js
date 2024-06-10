df2adf14dff8d8da6b87cf3f3f92943c6d01090a
rositapurw <167435979+rositapurw@users.noreply.github.com>
2024-06-11 05:58:36 +0800

    Create script.js

---
+++ module4-solution/script.js
@@ -0,0 +1,28 @@
+(function (window) {
+	var helloSpeaker = {};
+	helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (var i = 0; i < helloSpeaker.names.length; i++) {
+		var firstLetter = helloSpeaker.names[i].charAt(0).toLowerCase(); 
+		if (firstLetter === 'j') {
+			byeSpeaker.speakBye(helloSpeaker.names[i]);
+		} else {
+			helloSpeaker.speakHello(helloSpeaker.names[i]);
+		}
+	}
+	var speakHello = "Hello ";
+	helloSpeaker.speakHello = function () {
+		console.log(speakHello + helloSpeaker.names)
+	}
+	// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	// for (var i = 0; i < names.length; i++) {
+	// 	var firstLetter = names[i].charAt(0).toLowerCase();
+	// 	if (firstLetter === 'j') {
+	// 		byeSpeaker.speakBye(names[i]);
+ 	// 	} else {
+   	// 		helloSpeaker.speakHello(names[i]);
+ 	// 		}
+	// }
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
