41bdcae8042ac794d87d38487e3b686bf4f6ecc4
rositapurw <167435979+rositapurw@users.noreply.github.com>
2024-06-16 18:17:50 +0800

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,1 +1,9 @@
-helloSpeaker.speakGoodBye();
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
